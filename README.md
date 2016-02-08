c2390d5f2bf57cdb9a7d2edd57454464bbb705c1
Herman J. Radtke III <herman@hermanradtke.com>
2016-02-08 07:47:57 -0800

    Use Rust to configure carp C library
    - Introduce carp::Config struct. This has required me to allow failures
      on beta nd stable builds until `IpAddr` is stabilized in Rust 1.7
    - Add C level API to setup the global variables in carp C library
    - Remove unused files
    - Added an example program in `examples/basic.rs` which shows how to configure
      and then run carp.
    - Move README to README.md and mark project as non-standard GNU

---
+++ README.md
@@ -0,0 +1,132 @@
+# Common Address Redundancy Protocol
+
+carp-rs allows a couple of hosts to share common virtual IP addresses in order
+to provide automatic failover. It is a portable userland implementation of the
+secure and patent-free Common Address Redundancy Protocol (CARP, OpenBSD's
+alternative to the patents-bloated VRRP).
+
+Strong points of the CARP protocol are: very low overhead, cryptographically
+signed messages, interoperability between different operating systems and no
+need for any dedicated extra network link between redundant hosts.
+
+This project has been forked from https://github.com/jedisct1/UCarp and
+relicensed under the LGPL. The original BSD license has been moved to
+the COPYING.OLD file.
+
+[![Build Status](https://travis-ci.org/hjr3/carp-rs.svg?branch=master)](https://travis-ci.org/hjr3/carp-rs)
+
+## Compilation
+
+   * libpcap (http://www.tcpdump.org/) must be installed on your system, with
+   development files (headers).
+      * On Ubuntu: `apt-get install libtool autoconf gettext libpcap0.8 libpcap0.8-dev libpcap-dev sqlite3 libsqlite3-dev`
+   * Currently, the C code must first use autoconf before Rust can compile it. The traditional procedure:
+```
+$ libtoolize
+$ autoreconf -i
+$ ./configure
+```
+   * Once that is complete, you can run `cargo build`. This will build a static
+   `libcarp.a` library from the C code and build a shared Rust library that
+   links against `libcarp.a`.
+
+The original C software has been successfully tested on Linux 2.4, Linux 2.6, MacOS X,
+OpenBSD, MirBSD and NetBSD. That being said, I have made a number of changes as
+I integrate the existing code with Rust.
+
+## Examples
+
+There is a basic example at `examples/basic.rs`. To run this example:
+
+```
+sudo cargo run --example basic
+```
+
+You should see some output similar to:
+
+```
+2016-02-10T15:57:02.986205: [INFO] Using [eth0] as a network interface
+2016-02-10T15:57:02.986249: [WARNING] Warning: no script called when going up
+2016-02-10T15:57:02.986266: [WARNING] Warning: no script called when going down
+2016-02-10T15:57:02.987559: [INFO] Local advertised ethernet address is [08:00:27:4d:3c:39]
+2016-02-10T15:57:02.987983: [WARNING] Switching to state: BACKUP
+2016-02-10T15:57:06.001703: [WARNING] Switching to state: MASTER
+```
+
+Note: carp uses the pcap library which requires root for certain operations.
+This may change in the future.
+
+## Master Selection Process
+
+When carp first runs, it starts as a backup and listens to the network
+to determine if it should become the master.  If at any time more than
+three times the node's advertising interval (defined as the advertising
+base (seconds) plus a fudge factor, the advertising skew) passes without
+hearing a peer's CARP advertisement, the node will transition itself to
+being a master.
+
+Transitioning from backup to master means:
+
+   1. calling the specified callback to assign the vip to the local system
+   2. sending a gratuitous arp to the network to claim the vip
+   3. continuously sending CARP advertisements to the network every interval.
+
+Transitioning from master to backup means:
+
+   1. calling the specified callback to remove the vip from the local system
+
+To understand how carp works, it's important to note that the
+advertisement interval is not only used as the time in between which
+each CARP advertisement is sent by the master, but also as a priority
+mechanism where shorter (i.e. more frequent) is better.  The interval
+base and skew values are stored in the CARP advertisement and are used
+by other nodes to make certain decisions.
+
+By default, once a node becomes the master, it will continue on
+indefinitely as the master.  If you like/want/need this behavior, or don't
+have a preferred master, then choose the same interval on all hosts.
+If for whatever reason you were to choose different intervals on the
+hosts, then over time the one with the shortest interval would tend to
+become the master as machines are rebooted, after failures, etc.
+
+Also of note is a conflict resolution algorithm that in case a master
+hears another, equal (in terms of its advertised interval) master, the
+one with the lower IP address will remain master and the other will
+immediately demote itself.  This is simply to eliminate flapping and
+quickly determine who should remain master.  This situation should not
+happen very often but it can.
+
+If you want a "preferred" master to always be the master (even if another
+host is already the master), add the preempt switch and
+assign a shorter interval via the advertisement base and
+skew.  This will cause the preferred node to ignore a
+master who is advertising a longer interval and promote itself to master.
+The old master will quickly hear the preferred node advertising a shorter
+interval and immediately demote itself.
+
+In summary, a backup will become master if:
+
+   * no one else advertises for 3 times its own advertisement interval
+   * you specified --preempt and it hears a master with a longer interval
+
+and a master will become backup if:
+
+   *  another master advertises a shorter interval
+   *  another master advertises the same interval, and has a lower IP address
+
+## Authors
+
+The carp-rs project is based upon the hard work of many people on the original UCarp project.
+
+   * Frank DENIS <j at pureftpd dot org>
+   * Eric Evans <eevans at sym-link dot com> - maintainer of Debian packages.
+   * David H <dmalloc at users dot sf dot net> - maintainer of Fink packages.
+   * Richard Bellamy <richard dot bellamy at virgin dot net> - helped a lot with Solaris portability.
+   * Russell Mosemann - neutral mode and bug fixes.
+   * Dean Gaudet <dean at arctic dot org> - EINTR handling, log exec errors, --passfile.
+   * Steve Kehlet and Marcus Goller - fixed the bogus code that issued poisonous
+     gratuitous ARP, and improved the behavior when multiple nodes are started
+     with the same interval and skew.  Steve helped a lot on many things.
+   * Tim Niemeyer <niemeyer at kdo dot de> - Ensure remastering works when the
+     preferred master has its network connection flap.
+   * Serve Sireskin - --ignoreifstate option.
--- src/ucarp.c
+++ src/ucarp.c
@@ -21,63 +21,11 @@
 
 #include <config.h>
 #include "ucarp.h"
-#ifndef HAVE_GETOPT_LONG
-# include "bsd-getopt_long.h"
-#else
-# include <getopt.h>
-#endif
 #include "log.h"
-#include "daemonize.h"
-#include "ucarp_p.h"
 
 #ifdef WITH_DMALLOC
 # include <dmalloc.h>
 #endif
-
-static void usage(void)
-{
-    puts("\n" PACKAGE_STRING " - " __DATE__ "\n");
-    fputs(
-        "--interface=<if> (-i <if>): bind interface <if>\n"
-        "--srcip=<ip> (-s <ip>): source (real) IP address of that host\n"
-        "--mcast=<ip> (-m <ip>): multicast group IP address (default 224.0.0.18)\n"
-        "--vhid=<id> (-v <id>): virtual IP identifier (1-255)\n"
-        "--pass=<pass> (-p <pass>): password\n"
-        "--passfile=<file> (-o <file>): read password from file\n"
-        "--preempt (-P): becomes a master as soon as possible\n"
-        "--neutral (-n): don't run downscript at start if backup\n"
-        "--addr=<ip> (-a <ip>): virtual shared IP address\n"
-        "--help (-h): summary of command-line options\n"
-        "--advbase=<seconds> (-b <seconds>): advertisement frequency\n"
-        "--advskew=<skew> (-k <skew>): advertisement skew (0-255)\n"
-        "--upscript=<file> (-u <file>): run <file> to become a master\n"
-        "--downscript=<file> (-d <file>): run <file> to become a backup\n"
-        "--deadratio=<ratio> (-r <ratio>): ratio to consider a host as dead\n"
-        "--debug (-D): enable debug output\n"
-        "--shutdown (-z): call shutdown script at exit\n"
-        "--daemonize (-B): run in background\n"
-        "--ignoreifstate (-S): ignore interface state (down, no carrier)\n"
-        "--nomcast (-M): use broadcast (instead of multicast) advertisements\n"
-        "--facility=<facility> (-f): set syslog facility (default=daemon)\n"
-        "--xparam=<value> (-x): extra parameter to send to up/down scripts\n"
-        "\n"
-        "Sample usage:\n"
-        "\n"
-        "Manage the 10.1.1.252 shared virtual address on interface eth0, with\n"
-        "1 as a virtual address idenfitier, mypassword as a password, and\n"
-        "10.1.1.1 as a real permanent address for this host.\n"
-        "Call /etc/vip-up.sh when the host becomes a master, and\n"
-        "/etc/vip-down.sh when the virtual IP address has to be disabled.\n"
-        "\n"
-        "ucarp --interface=eth0 --srcip=10.1.1.1 --vhid=1 --pass=mypassword \\\n"
-        "      --addr=10.1.1.252 \\\n"
-        "      --upscript=/etc/vip-up.sh --downscript=/etc/vip-down.sh\n"
-        "\n\n"
-        "Please report bugs to ", stdout);
-    puts(PACKAGE_BUGREPORT ".\n");
-
-    exit(EXIT_SUCCESS);
-}
 
 static void init_rand(void)
 {
@@ -93,196 +41,123 @@ static void init_rand(void)
 #endif
 }
 
-static void die_mem(void)
+int set_vaddr(const char *_vaddr)
 {
-    logfile(LOG_ERR, "Out of memory");
-
-    exit(EXIT_FAILURE);
+    free(vaddr_arg);
+    if (inet_pton(AF_INET, _vaddr, &vaddr) == 0) {
+        logfile(LOG_ERR, "Invalid virtual address: [%s]", _vaddr);
+        return 1;
+    }
+    vaddr_arg = strdup(_vaddr);
+    return 0;
 }
 
-int libmain(int argc, char *argv[])
+int set_mcast(const char *mcast)
 {
-    int option_index = 0;
-    int fodder;
+    if (inet_pton(AF_INET, mcast, &mcastip) == 0) {
+        logfile(LOG_ERR, "Invalid address: [%s]", mcast);
+        return 1;
+    }
 
-    if (argc <= 1) {
-        usage();
-    }
-    inet_pton(AF_INET, DEFAULT_MCASTIP, &mcastip);
-    while ((fodder = getopt_long(argc, argv, GETOPT_OPTIONS, long_options,
-                                 &option_index)) != -1) {
-        switch (fodder) {
-        case 'h': {
-            usage();
-        }
-        case 'i': {
-            free(interface);
-            if ((interface = strdup(optarg)) == NULL) {
-                die_mem();
-            }
-            break;
-        }
-        case 's': {
-            if (inet_pton(AF_INET, optarg, &srcip) == 0) {
-                logfile(LOG_ERR, "Invalid address: [%s]", optarg);
-                return 1;
-            }
-            break;
-        }
-        case 'm': {
-            if (inet_pton(AF_INET, optarg, &mcastip) == 0) {
-                logfile(LOG_ERR, "Invalid address: [%s]", optarg);
-                return 1;
-            }
-            break;
-        }
-        case 'v': {
-            if (strtoul(optarg, NULL, 0) > 255 || strtol(optarg, NULL, 0) < 1) {
-                logfile(LOG_ERR, "vhid must be between 1 and 255.");
-                return 1;
-            }
-            vhid = (unsigned char) strtoul(optarg, NULL, 0);
-            break;
-        }
-        case 'p': {
-            free(pass);
-            if ((pass = strdup(optarg)) == NULL) {
-                die_mem();
-            }
-            break;
-        }
-        case 'o': {
-            char buf[512U];
-            char *p;
-            FILE *pw;
-            if ((pw = fopen(optarg, "r")) == NULL) {
-                logfile(LOG_ERR,
-                        "unable to open passfile %s for reading: %s",
-                        optarg, strerror(errno));
-                return 1;
-            }
-            if (fgets(buf, sizeof buf, pw) == NULL) {
-                logfile(LOG_ERR, "error reading passfile %s: %s", optarg,
-                        ferror(pw) ?
-                        strerror(errno) : "unexpected end of file");
-                return 1;
-            }
-            fclose(pw);
-            p = strchr(buf, '\n');
-            if (p != NULL) {
-                *p = 0;
-            }
-            if ((pass = strdup(buf)) == NULL) {
-                die_mem();
-            }
-            break;
-        }
-        case 'P': {
-            preempt = 1;
-            break;
-        }
-        case 'n': {
-            neutral = 1;
-            break;
-        }
-        case 'a': {
-            free(vaddr_arg);
-            if (inet_pton(AF_INET, optarg, &vaddr) == 0) {
-                logfile(LOG_ERR, "Invalid address: [%s]", optarg);
-                return 1;
-            }
-            vaddr_arg = strdup(optarg);
-            break;
-        }
-        case 'b': {
-            advbase = (unsigned char) strtoul(optarg, NULL, 0);
-            break;
-        }
-        case 'k': {
-            advskew = (unsigned char) strtoul(optarg, NULL, 0);
-            break;
-        }
-        case 'd': {
-            free(downscript);
-            if ((downscript = strdup(optarg)) == NULL) {
-                die_mem();
-            }
-            break;
-        }
-        case 'D': {
-            debug = 1;
-            break;
-        }
-        case 'u': {
-            free(upscript);
-            if ((upscript = strdup(optarg)) == NULL) {
-                die_mem();
-            }
-            break;
-        }
-        case 'r': {
-            dead_ratio = (unsigned int) strtoul(optarg, NULL, 0);
-            break;
-        }
-        case 'z': {
-            shutdown_at_exit = 1;
-            break;
-        }
-        case 'B': {
-            daemonize = 1;
-            break;
-        }
-        case 'S': {
-            ignoreifstate = 1;
-            break;
-        }
-        case 'f': {
-            int n = 0;
+    return 0;
+}
 
-            if (strcasecmp(optarg, "none") == 0) {
-                no_syslog = 1;
-                break;
-            }
-            while (facilitynames[n].c_name &&
-                   strcasecmp(facilitynames[n].c_name, optarg) != 0) {
-                n++;
-            }
-            if (facilitynames[n].c_name) {
-                syslog_facility = facilitynames[n].c_val;
-            } else {
-                logfile(LOG_ERR, "Unknown syslog facility: [%s]", optarg);
-            }
-            break;
-        }
-        case 'x': {
-            free(xparam);
-            if ((xparam = strdup(optarg)) == NULL) {
-                die_mem();
-            }
-            break;
-        }
-        case 'M': {
-            no_mcast = 1;
-            break;
-        }
-        default: {
-            usage();
-        }
-        }
+int set_srcip(const char *_srcip)
+{
+    if (inet_pton(AF_INET, _srcip, &srcip) == 0) {
+        logfile(LOG_ERR, "Invalid address: [%s]", _srcip);
+        return 1;
     }
-#ifndef SAVE_DESCRIPTORS
-    if (no_syslog == 0) {
-        openlog("ucarp", LOG_PID, syslog_facility);
+
+    return 0;
+}
+
+int set_vhid(unsigned char _vhid)
+{
+    if (_vhid > 255 || _vhid < 1) {
+        logfile(LOG_ERR, "vhid must be between 1 and 255.");
+        return 1;
     }
-#endif
-    if (interface == NULL || *interface == 0) {
+    vhid = (unsigned char) _vhid;
+    return 0;
+}
+
+int set_interface(const char* _interface)
+{
+    if (_interface == NULL) {
+        // Note: This requires root or it will segfault.
         interface = pcap_lookupdev(NULL);
         if (interface == NULL || *interface == 0) {
             logfile(LOG_ERR, "You must supply a network interface");
             return 1;
         }
-        logfile(LOG_INFO, "Using [%s] as a network interface", interface);
+    } else {
+        free(interface);
+        if ((interface = strdup(_interface)) == NULL) {
+            return 1;
+        }
     }
+    logfile(LOG_INFO, "Using [%s] as a network interface", interface);
+    return 0;
+}
+
+int set_password(const char *password)
+{
+    free(pass);
+    if ((pass = strdup(password)) == NULL) {
+        return 1;
+    }
+
+    return 0;
+}
+
+void set_advbase(unsigned char _advbase)
+{
+    advbase = _advbase;
+}
+
+void set_advskew(unsigned char _advskew)
+{
+    advskew = _advskew;
+}
+
+void set_dead_ratio(unsigned int _dead_ratio)
+{
+    dead_ratio = _dead_ratio;
+}
+
+void set_preempt(signed char _preempt)
+{
+    preempt = _preempt;
+}
+
+void set_neutral(signed char _neutral)
+{
+    neutral = _neutral;
+}
+
+void set_shutdown_at_exit(signed char _shutdown_at_exit)
+{
+    shutdown_at_exit = _shutdown_at_exit;
+}
+
+void set_ignoreifstate(signed char _ignoreifstate)
+{
+    ignoreifstate = _ignoreifstate;
+}
+
+void set_no_mcast(signed char _no_mcast)
+{
+    no_mcast = _no_mcast;
+}
+
+int libmain(struct Config *config)
+{
+#ifndef SAVE_DESCRIPTORS
+    if (no_syslog == 0) {
+        openlog("ucarp", LOG_PID, syslog_facility);
+    }
+#endif
     if (vhid == 0) {
         logfile(LOG_ERR, "You must supply a valid virtual host id");
         return 1;
@@ -313,7 +188,6 @@ int libmain(int argc, char *argv[])
         logfile(LOG_ERR, "Dead ratio can't be zero");
         return 1;
     }
-    dodaemonize();
     init_rand();
     if (docarp() != 0) {
         return 2;
